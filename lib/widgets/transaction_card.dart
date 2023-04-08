68165e4b02a51ded77591d14c44c044d4da32304
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-08 11:31:36 +0200

    9. Explore the Problem with Lists & StatefulWidgets

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show DateFormat;
 
 import '../models/transaction.dart';
 
-class TransactionCard extends StatelessWidget {
+class TransactionCard extends StatefulWidget {
   final Transaction transaction;
   final void Function({required String id}) deleteTransaction;
 
@@ -14,32 +16,55 @@ class TransactionCard extends StatelessWidget {
   });
 
   @override
+  State<TransactionCard> createState() => _TransactionCardState();
+}
+
+class _TransactionCardState extends State<TransactionCard> {
+  late Color _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    const availableColor = [
+      Colors.amber,
+      Colors.purple,
+      Colors.blue,
+      Colors.green,
+    ];
+
+    _backgroundColor = availableColor[Random().nextInt(4)];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 3,
       child: ListTile(
         leading: CircleAvatar(
           radius: 40,
+          backgroundColor: _backgroundColor,
           child: Padding(
             // This makes the Text Container Touch the CircleAvatar Line
             padding: const EdgeInsets.all(16),
             child: FittedBox(
               child: Text(
-                '\$${transaction.amount.toStringAsFixed(2)}',
+                '\$${widget.transaction.amount.toStringAsFixed(2)}',
               ),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.titleMedium,
         ),
-        subtitle: Text(DateFormat.yMMMd().format(transaction.date)),
+        subtitle: Text(DateFormat.yMMMd().format(widget.transaction.date)),
         // When we have more width, I want to show a button with a
         // text next to it.
         trailing: MediaQuery.of(context).size.width > 500
             ? TextButton.icon(
-                onPressed: () => deleteTransaction(id: transaction.id),
+                onPressed: () =>
+                    widget.deleteTransaction(id: widget.transaction.id),
                 icon: const Icon(Icons.delete_rounded),
                 label: const Text('Delete'),
                 style: TextButton.styleFrom(
@@ -47,7 +72,8 @@ class TransactionCard extends StatelessWidget {
                 ),
               )
             : IconButton(
-                onPressed: () => deleteTransaction(id: transaction.id),
+                onPressed: () =>
+                    widget.deleteTransaction(id: widget.transaction.id),
                 color: Theme.of(context).errorColor,
                 icon: const Icon(Icons.delete_rounded),
               ),
